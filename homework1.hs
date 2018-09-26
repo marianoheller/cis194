aa7e779c18baa87d9222830cfd461fbaf299caf2
Mariano Heller <mariano.heller@w3americas.com>
2018-09-26 17:22:21 -0300

    homeowrk 1

--- homework1.hs
+++ homework1.hs
@@ -6,6 +6,9 @@ toDigits :: Integer -> [Integer]
 toDigits 0 = []
 toDigits n
   | n < 0 = []
-  | otherwise = toDigits (n `div` 10) ++ n `mod` 10
+  | otherwise =  (toDigits (n `div` 10)) ++ [n `mod` 10]
 
 toDigitsRev :: Integer -> [Integer]
+toDigitsRev 0 = []
+toDigitsRev n = reverse (toDigits n)
+  where reverse (x:xs) = (reverse xs) ++ [x]
