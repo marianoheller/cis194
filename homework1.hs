297a4369c67574966db68ac4d7a8d05ee96fc55a
Mariano Heller <mariano.heller@w3americas.com>
2018-09-29 11:32:38 -0300

    hw 1

--- homework1.hs
+++ homework1.hs
@@ -1,13 +1,27 @@
 {-
   Homework N° 1
+  http://www.seas.upenn.edu/~cis194/hw/01-intro.pdf
 -}
 
+-- Exercise 1
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n
+  | n <= 0 = []
+  | otherwise = ((mod n 10) : (toDigitsRev (div n 10)))
 
 toDigits :: Integer -> [Integer]
-toDigits n
-  | n <= 0 = []
-  | otherwise = ((mod n 10) : (toDigits (div n 10)))
+toDigits n = reverse (toDigitsRev n)
 
-toDigitsRev :: Integer -> [Integer]
-toDigitsRev 0 = []
-toDigitsRev n = reverse (toDigits n)
+-- Exercise 2
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = zipWith (*) (take (length xs) (cycle [1,2])) xs
+
+-- Exercise 3
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum (map arrItem xs)
+  where arrItem x = sum (toDigits x )
+
+-- Exercise 4
+validate :: Integer -> Bool
+validate x = mod processedNumber 10 == 0
+  where processedNumber = sumDigits (doubleEveryOther (toDigits x))
